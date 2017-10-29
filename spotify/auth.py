6a3a9a18731adfb13791309c789dc8ea8e48082a
tanguy <lefloch.ta@gmail.com>
2017-10-29 17:00:33 -0500

    Deleting past folder

--- spotify/auth.py
+++
@@ -1,9 +0,0 @@
-import requests
-from constants import CLIENT_ID, CLIENT_SECRET
-
-DATA_PARAMS = {'grant_type' : 'client_credentials'}
-URL ='https://accounts.spotify.com/api/token'
-
-def getAuthToken():
-    response = requests.post(URL, data = DATA_PARAMS, auth = (CLIENT_ID, CLIENT_SECRET))
-    return response.json()['access_token']
